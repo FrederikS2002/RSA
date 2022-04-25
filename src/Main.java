10b296f35735840af96744f3aa89b24073cc39e8
FrederikS2002 <86200874+FrederikS2002@users.noreply.github.com>
2022-04-25 17:05:28 +0200

    small fixes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-        RSA myrsa = new RSA(9973,  9923);
+        RSA myrsa = new RSA(17, 19);
         System.out.println(myrsa.encrypt("abcd"));
-        System.out.println(myrsa.decrypt("43134124 8269417 28424867 16856707 "));
+        System.out.println(myrsa.decrypt("279 72 74 161 "));
     }
 }
